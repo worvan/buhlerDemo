331fced439e43c95b5d7d244d3a11cef8a1f7030
Jiří Antoš <worvanoso@gmail.com>
2023-05-23 07:19:13 +0200

    first version

---
+++ src/app/machine-status/machine-status.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-machine-status',
+  templateUrl: './machine-status.component.html',
+  styleUrls: ['./machine-status.component.scss']
+})
+export class MachineStatusComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/overview/overview.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-overview',
+  templateUrl: './overview.component.html',
+  styleUrls: ['./overview.component.scss']
+})
+export class OverviewComponent implements OnInit {
+
+  constructor() { }
+  private  machine: Array<{id: number, name: string}> = [];
+
+  ngOnInit(): void {
+  }
+
+}
